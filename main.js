3ea5f70bee657cbf0a0271574344d18835517852
Jason Lally <jason.lally@gmail.com>
2013-10-01 12:39:58 -0700

    add click through disclaimer

--- main.js
+++ main.js
@@ -8,3 +8,14 @@ $(function() {
         offset: -70
     });
 });
+
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+function redirect() {
+    window.location = "http://" + getParameterByName("redirect");
+}
